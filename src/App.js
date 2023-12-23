7bf78c1309533b5a0dbd4dbe2b72e19ba105cd00
Vu Huy <vulehuy0102@gmail.com>
2023-12-23 09:33:08 +0700

    conplete

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Profile from './components/Profile';
+import NameCard from'./components/NameCard';
+import Counter from './components/Couter';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <Profile/> */}
+        <Counter/>
+        <NameCard name="Bob" age={30} gender="male" />
+        <NameCard name="Alice" age={25} gender="female" />
       </header>
     </div>
   );
